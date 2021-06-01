8319cef8a461903bce25c6429b564c7677a101d3
winljm001 <819648983@qq.com>
2021-06-01 10:56:26 +0800

    feat: init

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Button, Input } from 'antd'
 
 const NotFound: React.FC = () => {
   return (
     <div>
+      <Button>test</Button>
+      <Input />
       <p>默认缺省页</p>
     </div>
   );
