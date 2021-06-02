55fd06fb56e5d8dbe2d47faa882178221117b57e
winljm001 <819648983@qq.com>
2021-06-02 18:27:25 +0800

    feat: add modal

--- src/pages/system-management/goods-management/index.tsx
+++ src/pages/system-management/goods-management/index.tsx
@@ -1,22 +1,36 @@
 import BaseCard from '@/components/BaseCard';
 import BaseFormWrap from '@/components/BaseFormWrap';
-import { FormInstance } from 'antd';
+import { FormInstance, Modal, Table } from 'antd';
 import React, { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import SpecForm from './components/spec-form';
 import SpuForm from './components/spu-form';
-
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+import { useBoolean, useToggle } from 'ahooks';
 const Index: React.FC = () => {
   const history = useHistory();
+  const [visible, { toggle }] = useToggle();
   const spuFormRef = useRef<FormInstance>();
   const specFormRef = useRef<FormInstance>();
-  const onSaveAction = () => {
+  /** 保存果品操作 */
+  const handleSaveAction = () => {
     const form1 = spuFormRef.current.validateFields();
     const form2 = specFormRef.current.validateFields();
     Promise.all([form1, form2]).then(([res1, res2]) => {
       console.log(res1, res2);
+      Modal.confirm({
+        title: '去添加sku列表',
+        icon: <ExclamationCircleOutlined />,
+        content: '商品创建成功，你需要去添加sku列表',
+        okText: '去添加sku列表',
+        cancelText: '取消',
+        onOk: () => {
+          toggle();
+        },
+      });
     });
   };
+  const handleAddSku = () => {};
   return (
     <BaseFormWrap
       actions={[
@@ -30,7 +44,7 @@ const Index: React.FC = () => {
           type: 'primary',
           children: '保存果品',
           onClick: () => {
-            onSaveAction();
+            handleSaveAction();
           },
         },
         {
@@ -38,7 +52,7 @@ const Index: React.FC = () => {
           ghost: true,
           children: '添加sku列表',
           onClick: () => {
-            console.log(123);
+            toggle();
           },
         },
       ]}>
@@ -48,6 +62,24 @@ const Index: React.FC = () => {
       <BaseCard title="规格信息">
         <SpecForm ref={specFormRef} />
       </BaseCard>
+      <Modal
+        maskClosable={false}
+        title="选择SKU"
+        okText="保存"
+        cancelText="取消"
+        visible={visible}
+        onCancel={() => toggle()}
+        onOk={handleAddSku}>
+        123
+        {/* <Table
+          bordered
+          rowSelection={{ selectedRowKeys: selectedKeys, onChange: handleChangeSelection }}
+          pagination={{ pageSize: 10 }}
+          rowKey={(record) => record.filter.join('')}
+          columns={columns}
+          dataSource={dataSource}
+        /> */}
+      </Modal>
     </BaseFormWrap>
   );
 };
