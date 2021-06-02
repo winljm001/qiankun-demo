c19e9f55c31f611284974091d85f722c2be4ef19
winljm001 <819648983@qq.com>
2021-06-02 12:01:32 +0800

    feat: form init

--- src/pages/system-management/goods-management/components/spec-form/index.tsx
+++ src/pages/system-management/goods-management/components/spec-form/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import JsonForm, { defineConfig, useForm } from '@/components/JsonForm';
+import { FormInstance } from 'antd';
+import React, { forwardRef, useImperativeHandle } from 'react';
 
-const Index: React.FC = () => {
-  return (
-    <div>
-      <p>spec-form</p>
-    </div>
-  );
-};
+interface SpuFormProps {
+  data?: any;
+}
+const SpuForm = forwardRef<Partial<FormInstance>, SpuFormProps>(({ data = null }, ref) => {
+  const [form] = useForm();
+  useImperativeHandle(ref, () => ({
+    ...form,
+  }));
+  const config = defineConfig({
+    form,
+    initialValues: data,
+    formItems: [
+      {
+        label: '商品名称',
+        name: 'name1',
+        rules: [{ required: true, message: '请输入商品名称' }],
+        control: {
+          controlType: 'INPUT',
+        },
+      },
+    ],
+  });
+  return <JsonForm {...config} />;
+});
 
-export default Index;
+export default SpuForm;
--- src/pages/system-management/goods-management/index.tsx
+++ src/pages/system-management/goods-management/index.tsx
@@ -1,22 +1,36 @@
 import BaseCard from '@/components/BaseCard';
 import BaseFormWrap from '@/components/BaseFormWrap';
-import React from 'react';
+import { FormInstance } from 'antd';
+import React, { useRef } from 'react';
+import { useHistory } from 'react-router-dom';
+import SpecForm from './components/spec-form';
+import SpuForm from './components/spu-form';
 
 const Index: React.FC = () => {
+  const history = useHistory();
+  const spuFormRef = useRef<FormInstance>();
+  const specFormRef = useRef<FormInstance>();
+  const onSaveAction = () => {
+    const form1 = spuFormRef.current.validateFields();
+    const form2 = specFormRef.current.validateFields();
+    Promise.all([form1, form2]).then(([res1, res2]) => {
+      console.log(res1, res2);
+    });
+  };
   return (
     <BaseFormWrap
       actions={[
         {
           children: '返回',
           onClick: () => {
-            console.log(123);
+            history.goBack();
           },
         },
         {
           type: 'primary',
           children: '保存果品',
           onClick: () => {
-            console.log(123);
+            onSaveAction();
           },
         },
         {
@@ -29,10 +43,10 @@ const Index: React.FC = () => {
         },
       ]}>
       <BaseCard title="SPU信息">
-        <p>Home</p>
+        <SpuForm ref={spuFormRef} />
       </BaseCard>
       <BaseCard title="规格信息">
-        <p>Home</p>
+        <SpecForm ref={specFormRef} />
       </BaseCard>
     </BaseFormWrap>
   );
