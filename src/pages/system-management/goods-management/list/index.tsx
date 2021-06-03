c461cef1af9036459cd169a18134cafe00b275f7
winljm001 <819648983@qq.com>
2021-06-03 17:46:15 +0800

    feat: 添加Authorization

--- src/pages/system-management/goods-management/list/index.tsx
+++ src/pages/system-management/goods-management/list/index.tsx
@@ -3,34 +3,28 @@ import { Form, Table } from 'antd';
 import { useAntdTable } from 'ahooks';
 import { PaginatedParams } from 'ahooks/lib/useAntdTable';
 import SearchForm from './search-form/index';
-interface Item {
-  name: {
-    last: string;
-  };
-  email: string;
-  phone: string;
-  gender: 'male' | 'female';
-}
+import { pageCommodity } from '@/services/commodityService/mods/commodity/pageCommodity';
+// interface Item {
+//   name: {
+//     last: string;
+//   };
+//   email: string;
+//   phone: string;
+//   gender: 'male' | 'female';
+// }
 
-interface Result {
-  total: number;
-  list: Item[];
-}
+// interface Result {
+//   total: number;
+//   list: Item[];
+// }
 
-const getTableData = ({ current, pageSize }: PaginatedParams[0], formData: Object): Promise<Result> => {
-  let query = `page=${current}&size=${pageSize}`;
-  Object.entries(formData).forEach(([key, value]) => {
-    if (value) {
-      query += `&${key}=${value}`;
-    }
+const getTableData = ({ current, pageSize }: PaginatedParams[0], formData: Object) => {
+  const params = { pageCurrent: current, pageSize, ...formData };
+
+  return pageCommodity({ queryParams: params }).then((res) => {
+    console.log(res);
+    return { total: 0, list: [] };
   });
-
-  return fetch(`https://randomuser.me/api?results=55&${query}`)
-    .then((res) => res.json())
-    .then((res) => ({
-      total: res.info.results,
-      list: res.results,
-    }));
 };
 
 const AntdTable = () => {
