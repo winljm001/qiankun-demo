5f3217be58c65da207468bd57b8a79f909d9838a
LING_ZI_QING <onlylingstudio@gmail.com>
2021-06-04 15:14:56 +0800

    feat: new api template

--- api-template.ts
+++ api-template.ts
@@ -1,111 +1,73 @@
 /* eslint-disable @typescript-eslint/explicit-member-accessibility */
-/* eslint-disable max-params */
-import { CodeGenerator, Interface } from 'pont-engine';
+import { Interface, CodeGenerator } from 'pont-engine';
 
-function formatPath(rawPath: string) {
-  const reg = /\{(\w+)\}/; // 匹配{something}
-  const res = reg.exec(rawPath);
-  if (res) {
-    const index = res.index;
-    const params = res[1];
-    return [rawPath.substr(0, index) + '$' + rawPath.substr(index), params];
-  }
-  return [rawPath, ''];
-}
+const REQUEST_PARAM_KEY = 'Params';
 
-function getRequestParams(params: string) {
-  if (!params) {
-    return '';
-  }
-  return `, ${params}: string | number`;
-}
-
-function getParamsShapeCode(
-  queryParams: string,
-  bodyParams: string,
-  showQueryParams: boolean,
-  showBodyParams: boolean,
-) {
-  const paramsInterface = `
-  interface IParams{
-    ${showQueryParams ? queryParams : ''}
-    ${showBodyParams ? bodyParams : ''}
-  }
-  `;
-  const propsName = [];
-  if (showQueryParams) {
-    propsName.push('queryParams');
-  }
-  if (showBodyParams) {
-    propsName.push('bodyParams');
-  }
-  const paramsDestructureStr = `{${propsName.join(',')}}`;
-  return [paramsInterface, paramsDestructureStr];
-}
-
-function getParamsNeeded(showQueryParams: boolean, showBodyParams: boolean) {
-  const params = [];
-  if (showQueryParams) {
-    params.push(`queryParams: restParamsData[0]`);
-  }
-  if (showBodyParams && !showQueryParams) {
-    params.push(`bodyParams: restParamsData[0]`);
-  }
-  if (showBodyParams && showQueryParams) {
-    params.push(`bodyParams: restParamsData[1]`);
-  }
-  return params.join(',');
-}
-
+/**
+ * 格式化为常量的字符串
+ * @example 'saveSomeData' -> 'SAVE_SOME_DATA'
+ */
+const formatConstStr = (s: string) => s.replace(/\B([A-Z])/g, '_$1').toUpperCase();
 export default class MyGenerator extends CodeGenerator {
   getInterfaceContent(inter: Interface) {
-    const queryParamsType = 'IQueryParams';
-    const bodyParmas = inter.getBodyParamsCode();
-    const queryParams = inter.getParamsCode(queryParamsType);
-    const hasQueryParams = inter.parameters.filter(({ in: paramsIn }) => paramsIn === 'query').length > 0;
-    const queryParamsTmp = `queryParams:${queryParamsType}`;
-    const bodyParamsTmp = `bodyParams:${bodyParmas}`;
-    const [paramsInterface, paramsDestructureStr] = getParamsShapeCode(
-      queryParamsTmp,
-      bodyParamsTmp,
-      hasQueryParams,
-      !!bodyParmas,
-    );
-    const [path, params] = formatPath(inter.path);
-    const requestParamsCode = [bodyParmas ? 'data: bodyParams' : '', hasQueryParams ? 'params: queryParams' : '']
-      .filter(Boolean)
-      .join(',\n');
+    const method = inter.method.toUpperCase();
+    const paramsCode = inter.getParamsCode(REQUEST_PARAM_KEY, this.surrounding);
+    const paramList = inter.getParamList().filter((param) => param.paramType);
+    const hasBody = paramList.filter((p) => p.paramKey === 'body');
+    const APIUpName = inter.name.replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
+    const paramsTypeName = `${APIUpName}Params`;
+    const paramsTypeCode = `${REQUEST_PARAM_KEY}${hasBody.length ? ` & ${hasBody[0].paramType}` : ''}`;
+    const requestParamsStr = `params: ${paramsTypeName}, headers?:any`;
+    const useQueryKeyName = formatConstStr(`Use${APIUpName}Key`);
+    const useQueryKeyValue = `${inter.path}_${method}`;
+
     return `
-    /**
-     * @desc ${inter.description}
-     */
-    import request from '@/utils/request'
-    export ${queryParams}
-    export ${paramsInterface}
-    export function ${inter.name}(${paramsDestructureStr}:IParams = {} as IParams${getRequestParams(params)}) {
-      return request<${inter.responseType}>({
-        url: \`${path}\`,
-        method: '${inter.method}',
-        ${requestParamsCode}
-      });
-    }
-    interface RqParams {
-      queryKey: any;
-      pageParam?: any;
-    }
-    // 需要和react-query一起使用
-    export async function ${inter.name}Rq(params?: RqParams) {
-      const [_, ...restParamsData] = params.queryKey;
-      const fetchParams: IParams = {
-        ${getParamsNeeded(hasQueryParams, !!bodyParmas)}
-      }
-      try {
-        const res = await ${inter.name}(fetchParams);
-        return res
-      } catch (error) {
-        throw new Error(error)
-      }
-    }
+// ${inter.description} 接口文件
+// import { useQuery } from 'react-query'
+import * as request from '@/utils/fetch'
+
+/**
+ * @description ${inter.description} 接口 URL 参数/GET
+ */
+export ${paramsCode}
+
+/**
+ * @description ${inter.description} 接口参数
+ */
+export type ${paramsTypeName} = ${paramsTypeCode}
+
+/**
+ * @description ${inter.description} 接口
+ */
+export const ${inter.name} = (${requestParamsStr}) => {
+  return request.request<${inter.responseType}>({...request.buildOptions('${inter.path}', params, '${method}'), headers})
+}
+
+/**
+ * @description ${inter.description} hooks 默认的 key
+ */
+export const ${useQueryKeyName} = '${useQueryKeyValue}';
+
+// export const ${inter.name}Query = ({ queryKey }: {queryKey:any[]}) => {
+//   const [,params] = queryKey;
+//   return ${inter.name}(params);
+// }
+
+// /**
+//  * @description ${inter.description} hooks
+//  */
+// export const use${APIUpName} = (${requestParamsStr}, key = '${useQueryKeyName}') => {
+//   // 修正数据
+//   if(typeof headers === 'string') {
+//     key = headers;
+//     headers = null;
+//   }
+
+//   return useQuery({
+//     queryKey: [key, params],
+//     queryFn: () => ${inter.name}(params, headers),
+//   })
+// }
    `;
   }
 }
