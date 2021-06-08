3bcc1576cacc71661ab7bbf0127f04a20372604f
13008352446 <1737666820@qq.com>
2021-06-08 13:45:58 +0800

    feat:sku编辑页

--- src/pages/system-management/goods-management/sku-management/components/edit/index.tsx
+++ src/pages/system-management/goods-management/sku-management/components/edit/index.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Fruit from '../fruit/index'
+import Food from '../food/index'
 
-const Index: React.FC = () => {
+const Index: React.FC = (props) => {
+    // const visible = useState(false)
     return (
         <div>
             <Fruit />
+            <Food />
         </div>
     );
 };
--- src/pages/system-management/goods-management/sku-management/components/fruit/index.tsx
+++ src/pages/system-management/goods-management/sku-management/components/fruit/index.tsx
@@ -1,27 +1,124 @@
-import React, { useState } from 'react';
-import { Modal, Button } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Modal, Switch, Form, Button, Input, Select, Cascader } from 'antd';
+import { listUnitOptions } from '@/services/commodityService/mods/commoditySku/listUnitOptions'
+const { Option } = Select;
+
+
+import styles from './style.module.less'
 
 
 const Index: React.FC = () => {
-    const [visible, setVisible] = useState(false);
+    const [visible, setVisible] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [options, setOptions] = useState([])
+    const [weightArr, setweightArr] = useState([])
+
+
+    // 生命周期请求数据
+    useEffect(() => {
+        listUnitOptions({ commodityTypeId: 5 })
+            .then((res) => {
+                const data = res.data
+                console.log(data);
+                setweightArr(data)
+            }).catch((err) => {
+                console.log(err);
+            })
+        listUnitOptions({ commodityTypeId: 1 })
+            .then((res) => {
+                const data = res.data
+                // console.log(data);
+
+                setOptions(data)
+            }).catch((err) => {
+                console.log(err);
+            })
+    }, [])
+
+    // Form实例化
+    const [form] = Form.useForm()
+
+
+    const selectAfter = (
+        <Form.Item className={styles.select_after} name='skuCompany'>
+            <Select defaultValue="单位" style={{ width: 80 }}  >
+                {
+                    weightArr.map(item => {
+                        return <Option key={item.value} value={`${item.value}`}>{item.label}</Option>
+                    })
+                }
+            </Select>
+        </Form.Item>
+    )
+
+    // const options = [
+    //     {
+    //         value: 'zhejiang',
+    //         label: 'Zhejiang',
+    //         children: [
+    //             {
+    //                 value: 'hangzhou',
+    //                 label: 'Hangzhou',
+    //                 children: [
+    //                     {
+    //                         value: 'xihu',
+    //                         label: 'West Lake',
+    //                     },
+    //                 ],
+    //             },
+    //         ],
+    //     },
+    // ];
+    const onOk = () => {
+        form
+            .validateFields()
+            .then((values) => {
+                console.log(values);
+                form.resetFields();
+                console.log(values);
+                setVisible(false)
+            })
+            .catch((info) => {
+                setLoading(true)
+                console.log('Validate Failed:', info);
+            });
+    }
+
     return (
-        <>
-            <Button type="primary" onClick={() => setVisible(true)}>
-                Open Modal of 1000px width
-      </Button>
-            <Modal
-                title="Modal 1000px width"
-                centered
-                visible={visible}
-                onOk={() => setVisible(false)}
-                onCancel={() => setVisible(false)}
-                width={1000}
-            >
-                <p>some contents...</p>
-                <p>some contents...</p>
-                <p>some contents...</p>
-            </Modal>
-        </>
+        <Modal
+            okText="保存"
+            cancelText="取消"
+            title="编辑sku"
+            centered
+            visible={visible}
+            // onOk={onOk}
+            // onCancel={() => setVisible(false)}
+            width={640}
+            footer={[
+                <Button key="back" disabled={loading} onClick={() => setVisible(false)} >
+                    取消
+                </Button>,
+                <Button key="submit" type="primary" loading={loading} onClick={onOk} >
+                    保存
+                </Button>
+            ]}
+        >
+            <Form form={form} name="basic" className={styles.formBox}>
+                {/* 一 */}
+                <Form.Item label="sku净重" name="weight" rules={[{ required: true, message: '请输入净重!' }]} className={styles.weight}>
+                    <Input addonAfter={selectAfter} />
+                </Form.Item>
+                {/* 二 */}
+                <Form.Item label="sku单位" name="Company" rules={[{ required: true, message: '请选择单位!' }]}>
+                    <Cascader style={{ width: '70%' }} options={options} placeholder="请选择" />
+                </Form.Item>
+                {/* 三 */}
+                <Form.Item label="状态" name="switch" valuePropName="checked" className={styles.switch}>
+                    <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked={true} />
+                </Form.Item>
+
+            </Form >
+        </Modal >
     );
 };
 
