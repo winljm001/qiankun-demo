4e593bbe6ac705b43d41f2e176506c50b597e1c1
changhua.li <1275305684@qq.com>
2021-06-08 14:21:20 +0800

    chore:update services

--- src/services/authService/baseClass.ts
+++ src/services/authService/baseClass.ts
@@ -1,1 +1,32 @@
-export const authService = {}
+class ApiResult {
+  /** data */
+  data = []
+
+  /** errCode */
+  errCode = undefined
+
+  /** errMsg */
+  errMsg = ''
+}
+
+class AuthDTO {
+  /** authId */
+  authId = undefined
+
+  /** authKey */
+  authKey = ''
+
+  /** authName */
+  authName = ''
+
+  /** authPath */
+  authPath = ''
+
+  /** leaf */
+  leaf = undefined
+}
+
+export const authService = {
+  ApiResult,
+  AuthDTO
+}
