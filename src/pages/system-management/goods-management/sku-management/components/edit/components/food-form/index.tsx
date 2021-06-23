46759f9dea929b7136fa45ceeffadf8ae9f07840
winljm001 <819648983@qq.com>
2021-06-23 11:09:13 +0800

    fix: style

--- src/pages/system-management/goods-management/sku-management/components/edit/components/food-form/index.tsx
+++ src/pages/system-management/goods-management/sku-management/components/edit/components/food-form/index.tsx
@@ -5,7 +5,6 @@ import { useForm } from '@/components/JsonForm'
 import BaseCheckboxByFetch from '@/components/BaseCheckboxByFetch'
 import { listPropertyOptions } from '@/services/commodityService/mods/commoditySku/listPropertyOptions'
 import { FormRef } from '../../index'
-import styles from './style.module.less'
 
 type IProps = {
   initialValues: defs.commodityService.SkuDetails
@@ -28,22 +27,14 @@ const FoodForm = forwardRef<FormRef, IProps>(({ initialValues }, ref) => {
       .catch((err) => {})
   }, [])
   return (
-    <Form form={form} initialValues={{ status: true, ...initialValues }} name="basic" className={styles.formBox}>
-      <Form.Item
-        label="最小单位"
-        name="unitType"
-        rules={[{ required: true, message: '请选择单位!' }]}
-        className={styles.company}>
+    <Form form={form} layout="vertical" initialValues={{ status: true, ...initialValues }} name="basic">
+      <Form.Item label="最小单位" name="unitType" rules={[{ required: true, message: '请选择单位!' }]}>
         <Select options={skuUnitOptions} placeholder="请选择" />
       </Form.Item>
-      <Form.Item
-        label="副单位"
-        name="totalType"
-        rules={[{ required: true, message: '请选择单位!' }]}
-        className={styles.copyCompany}>
+      <Form.Item label="副单位" name="totalType" rules={[{ required: true, message: '请选择单位!' }]}>
         <Select options={totalTypeOptions} placeholder="请选择" />
       </Form.Item>
-      <Form.Item shouldUpdate>
+      <Form.Item shouldUpdate noStyle>
         {({ getFieldValue }) => {
           let unitTypeValue = getFieldValue('unitType')
           const newUnitTypeValue = skuUnitOptions.filter((item) => {
@@ -63,8 +54,7 @@ const FoodForm = forwardRef<FormRef, IProps>(({ initialValues }, ref) => {
               rules={[
                 { required: true, message: '请填写换算比率!' },
                 { pattern: /(^[1-9](\d+)?(\.\d{1,2})?$)|(^\d\.\d{1,2}$)/, message: '请输入正确数值' },
-              ]}
-              className={styles.ratio}>
+              ]}>
               <Input
                 addonBefore={toNewTotalTypeValue ? `一${toNewTotalTypeValue}=` : ''}
                 suffix={toNewUnitTypeValue ? `${toNewUnitTypeValue}` : ''}
@@ -83,7 +73,7 @@ const FoodForm = forwardRef<FormRef, IProps>(({ initialValues }, ref) => {
           }}
         />
       </Form.Item>
-      <Form.Item className={styles.switch} label="状态" name="status" valuePropName="checked">
+      <Form.Item label="状态" name="status" valuePropName="checked">
         <Switch checkedChildren="开启" unCheckedChildren="关闭" defaultChecked={true} />
       </Form.Item>
     </Form>
