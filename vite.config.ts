faafc19eea0d33f8b270d012b061f55ad6447ea3
LING_ZI_QING <onlylingstudio@gmail.com>
2021-09-02 17:19:53 +0800

    feat: 初始化

--- vite.config.ts
+++ vite.config.ts
@@ -5,50 +5,54 @@ import styleImport from 'vite-plugin-style-import'
 import { envConfig } from './src/config'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        modifyVars: { '@primary-color': '#0065FE' },
-        additionalData: `@import '@/assets/styles/variables.less';`,
+export default defineConfig(({ mode }) => {
+  // vite preview模式下mode无法修改，因此需要用dev兜底
+  const { apiHost, baseUrl } = envConfig[mode] || envConfig.dev
+  return {
+    base: baseUrl,
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          modifyVars: { '@primary-color': '#0065FE' },
+          additionalData: [`@import '@/assets/styles/variable.less';`],
+        },
       },
     },
-  },
-  plugins: [
-    styleImport({
-      libs: [
-        {
-          libraryName: 'antd',
-          esModule: true,
-          resolveStyle: (name) => {
-            return `antd/es/${name}/style/index`
+    plugins: [
+      styleImport({
+        libs: [
+          {
+            libraryName: 'antd',
+            esModule: true,
+            resolveStyle: (name) => {
+              return `antd/es/${name}/style/index`
+            },
           },
+        ],
+      }),
+      reactRefresh(),
+    ],
+    server: {
+      https: false,
+      host: true,
+      proxy: {
+        [`${baseUrl}graphql`]: {
+          target: apiHost,
+          changeOrigin: true,
         },
-      ],
-    }),
-    reactRefresh(),
-  ],
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/pitaya-app/api': {
-        target: envConfig.dev.apiHost,
-        changeOrigin: true,
-        rewrite(path) {
-          return path
+        '/_files': {
+          target: 'https://durian-test.hjgpscm.com',
+          changeOrigin: true,
+          ws: false,
+          secure: true,
         },
       },
-      '/_files': {
-        target: envConfig.dev.uploadHost,
-        changeOrigin: true,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  base: '/', // 设置公共基础路径，如果构建时有这个必要的话
+  }
 })
